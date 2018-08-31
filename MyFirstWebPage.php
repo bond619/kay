1042ee06f2f09446ed838f8ca1e5080044cefba4
bond619 <31978635+bond619@users.noreply.github.com>
2018-08-31 01:03:34 -0500

    Add files via upload

---
+++ MyFirstWebPage.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title> edgy boi </title>
+	</head>
+	<body style="background-color:pink;"> 
+
+	<p><i><h1 style="color:MediumOrchid;">you're only invited if you're edgy</h1></i></p>
+	<ul>	
+		<li><b><i><h2 style="color:FireBrick;">i'm edgy</h2></i></b></li>
+		<li><i><h2 style="color:DimGray;">i'm not edgy</h2></i></li>
+	</ul>
+	<form action="ha.php" method="post">
+  First name:<br>
+  <input type="text" name="firstname"><br>
+  Last name:<br>
+  <input type="text" name="lastname">
+  <input type="submit">
+</form>
+	<a href=https://www.youtube.com/watch?v=W8EQM1ekmd4> <i>yeet</i></a>
+	<p><img src="f0aa98cda5d143230e4e83f17fc96150.jpg" alt="you suck"/></p>
+	</body>
+</html>
---
+++ ha.php
@@ -0,0 +1,7 @@
+<?php 
+
+echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; 
+
+echo and thats a real gay name, $_POST["name"];
+?>
